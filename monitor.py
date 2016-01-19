f86d8b63ee6ef1721864b5fad0336c0b68d3cf95
phillipdwright <wright.phillip@gmail.com>
2016-01-19 16:14:25 -0500

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -11,27 +11,27 @@ mxp = soup.find(id="mxp")
 mxpdivs = mxp.find_all('td')
 todays_status = mxpdivs[1].find('img').get('src').split('/')[-1]
 
-# See if the server has been fixed
+# See if the server has been fixed.
 fixed = False
 if todays_status == 'tick-circle.png':
     fixed = True
 
-# 
+# Send the email if the server has been fixed.
 if fixed:
-    # Add the project path to the directory
+    # Add the project path to the directory.
     import sys
     newpath = r'C:\Users\phiwri\AppData\Local\Programs\Python'
     newpath += r'\Python35-32\Lib\site-packages\pinky_notify'
     sys.path.append(newpath)
     
-    # Check to see if it's already done
+    # Check to see if it's already done.
     from already_done import already_done
     
     if not already_done:
-        # Send the email
+        # Send the email.
         import send_pinky_email
         
-        # Change the already_done variable
+        # Change the already_done variable.
         with open(newpath + r'\already_done.py', 'w') as file:
             file.write('already_done = True')
 
